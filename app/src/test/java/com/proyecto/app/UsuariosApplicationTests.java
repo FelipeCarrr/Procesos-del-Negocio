8ff10bbc8204934b79600f4e691a23b7eb120923
SCIS3-PC23\SCIS3-23 <afnavarroc@ufpso.edu.co>
2022-03-16 19:52:13 -0500

    Primer test con jUnit para el metodo verSaludo de la clase saludoController

---
+++ app/src/test/java/com/proyecto/app/UsuariosApplicationTests.java
@@ -0,0 +1,19 @@
+package com.proyecto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Autowired
+	private SaludoController saludo;
+	@Test
+	void contextLoads() {
+		assertEquals(saludo.verSaludo( "Mundo").getContenido(), "Hola, Mundo!");
+	}
+
+}
