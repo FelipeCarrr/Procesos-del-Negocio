ca8d0442219ec1be757fce1c1b7273f4ea82bc41
FelipeCarrr <101758804+FelipeCarrr@users.noreply.github.com>
2022-04-07 20:02:10 -0500

    Add files via upload
    Implementacion de SpringSecurity y Hash de contraseña

---
+++ src/test/java/com/proyecto/app/UserRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.proyecto.app;
+
+import com.proyecto.app.entity.User;
+import com.proyecto.app.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setEmail("afnavarroc@ufpso.edu.co");
+        user.setPassword(passwordEncoder.encode("123456"));
+        user.setFirstName("Andres");
+        user.setLastName("Navarro");
+
+        User savedUser = repository.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertEquals(user.getEmail(), existUser.getEmail());
+    }
+}
