b2dc71452ce378f1632fcdf1dc9aac82deb04db1
FelipeCarrr <101758804+FelipeCarrr@users.noreply.github.com>
2022-05-19 19:56:00 -0500

    Add files via upload
    Implementacion de modal y alerts con js

--- src/main/resources/static/js/local.js
+++ src/main/resources/static/js/local.js
@@ -43,8 +43,8 @@ function listar(){
           '<td>'+usuario.email+'</td>'+
           '<td>'+
             '<button type="button" class="btn btn-outline-danger" onclick="eliminarUsuario(\''+usuario.id+'\')"><i class="fa-solid fa-user-minus"></i></button>'+
-            '<a href="#" onclick"traerModificarUsuario(\''+usuario.id+'\')" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
-            '<a href="visualizar.html" class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
+            '<a href="#" onclick="traerModificarUsuario(\''+usuario.id+'\')" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
+            '<a href="#" onclick="verUsuario(\''+usuario.id+'\')"class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
           '</td>'+
         '</tr>';
          }
@@ -84,6 +84,7 @@ function eliminarUsuario(id){
     .then(response => response.json())
     .then(function(data){
         listar()
+        alertas("Se ha Eliminado el usuario Exitosamente!", 2)
     })
 }
 
@@ -95,7 +96,7 @@ function traerModificarUsuario(id){
             'Content-Type': 'application/json'
         },
     }
-    fetch("api/users"+id,settings)
+    fetch("api/users/"+id,settings)
     .then(response => response.json())
     .then(function(usuario){
         
@@ -104,7 +105,7 @@ function traerModificarUsuario(id){
         if(usuario){
             cadena = '<div class="p-3 mb-2 bg-secondary text-white"><h1 class="display-5"><i class="fa-solid fa-user-pen"></i>Modificar Usuarios</h1></div>'+
             '<form action="" method="post" id="myForm">'+
-                'input type="hidden" name="id" id="id" value="'+usuario.id+'">'+
+                '<input type="hidden" name="id" id="id" value="'+usuario.id+'">'+
                 '<label for="firstName" class="form-label">First Name</label>'+
                 '<input type="text" name="firstName" class="form-control" id="firstName" required value="'+usuario.firstName+'"> <br>'+
                 '<label for="lastName" class="form-label">Last Name</label>'+
@@ -116,7 +117,9 @@ function traerModificarUsuario(id){
                 '<button type="button" class="btn btn-outline-warning" onclick="modificarUsuario(\''+usuario.id+'\')">Modificar</button>'+
             '</form>';
          }
-         document.getElementById("datos").innerHTML=cadena;
+         document.getElementById("contentModal").innerHTML=cadena;
+         var myModal = new bootstrap.Modal(document.getElementById('modalUsurio'))
+         myModal.toggle();
         
     })
 }
@@ -127,7 +130,7 @@ async function modificarUsuario(id){
     for(var [k, v] of formData){//convertimos los datos a json
         jsonData[k] = v;
     }
-    const request = await fetch("api/users"+id, {
+    const request = await fetch("api/users/"+id, {
         method: 'PUT',
         headers:{
             'Accept': 'application/json',
@@ -136,5 +139,59 @@ async function modificarUsuario(id){
         body: JSON.stringify(jsonData)
     });
     listar();
-    document.getElementById("datos").innerHTML = '';
+    alertas("Se ha modificado el usuario Exitosamente!", 1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsurio')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+
+function verUsuario(id){
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    fetch("api/users/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+        
+        var cadena='';
+        
+        if(usuario){
+            cadena = '<div class="p-3 mb-2 bg-secondary text-white"><h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Visualizar Usuario Usuarios</h1></div>'+
+            '<ul class="list-group">'+
+            '<li class="list-group-item">Nombre:'+usuario.firstName+'</li>'+
+            '<li class="list-group-item">Apellido: '+usuario.lastName+'</li>'+
+            '<li class="list-group-item">Email: '+usuario.email+'</li>'+
+            '</ul>'+
+            '<br>'
+         }
+         document.getElementById("contentModal").innerHTML=cadena;
+         var myModal = new bootstrap.Modal(document.getElementById('modalUsurio'))
+         myModal.toggle();
+        
+    })
+}
+
+function alertas(mensaje,tipo){
+    var color=""; 
+    if(tipo == 1 ){//success verde
+        color = "success"
+
+    }
+    else{//danger rojo
+        color = "danger"
+
+    }
+    var alerta = '<div class="alert alert-'+color+' alert-dismissible fade show" role="alert">'+
+                   '<strong><i class="fa-solid fa-triangle-exclamation"></i></strong>'+ 
+                   mensaje+
+                   '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'+
+                 '</div>';
+    document.getElementById("datos").innerHTML=alerta;
+
+
 }
--- target/classes/static/js/local.js
+++ target/classes/static/js/local.js
@@ -42,9 +42,9 @@ function listar(){
           '<td>'+usuario.lastName+'</td>'+
           '<td>'+usuario.email+'</td>'+
           '<td>'+
-            '<button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-user-minus"></i></button>'+
-            '<a href="modificar.html" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
-            '<a href="visualizar.html" class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
+            '<button type="button" class="btn btn-outline-danger" onclick="eliminarUsuario(\''+usuario.id+'\')"><i class="fa-solid fa-user-minus"></i></button>'+
+            '<a href="#" onclick="traerModificarUsuario(\''+usuario.id+'\')" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
+            '<a href="#" onclick="verUsuario(\''+usuario.id+'\')"class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
           '</td>'+
         '</tr>';
          }
@@ -72,4 +72,126 @@ async function sendData(path){
     });
     myForm.reset();
     console.log(await request.text())
+}
+function eliminarUsuario(id){
+    var settings={
+        method: 'DELETE',
+        headers:{
+            'Accept': 'application/json',
+        },
+    }
+    fetch("api/users/"+id,settings)
+    .then(response => response.json())
+    .then(function(data){
+        listar()
+        alertas("Se ha Eliminado el usuario Exitosamente!", 2)
+    })
+}
+
+function traerModificarUsuario(id){
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    fetch("api/users/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+        
+        var cadena='';
+        
+        if(usuario){
+            cadena = '<div class="p-3 mb-2 bg-secondary text-white"><h1 class="display-5"><i class="fa-solid fa-user-pen"></i>Modificar Usuarios</h1></div>'+
+            '<form action="" method="post" id="myForm">'+
+                '<input type="hidden" name="id" id="id" value="'+usuario.id+'">'+
+                '<label for="firstName" class="form-label">First Name</label>'+
+                '<input type="text" name="firstName" class="form-control" id="firstName" required value="'+usuario.firstName+'"> <br>'+
+                '<label for="lastName" class="form-label">Last Name</label>'+
+                '<input type="text" name="lastName" class="form-control" id="lastName" required value="'+usuario.lastName+'"> <br>'+
+                '<label for="email" class="form-label">Email</label>'+
+                '<input type="email" name="email" class="form-control" id="email" required value="'+usuario.email+'"> <br>'+
+                '<label for="password" class="form-label">Password</label>'+
+                '<input type="password" id="password" class="form-control" name="password" required> <br>'+
+                '<button type="button" class="btn btn-outline-warning" onclick="modificarUsuario(\''+usuario.id+'\')">Modificar</button>'+
+            '</form>';
+         }
+         document.getElementById("contentModal").innerHTML=cadena;
+         var myModal = new bootstrap.Modal(document.getElementById('modalUsurio'))
+         myModal.toggle();
+        
+    })
+}
+async function modificarUsuario(id){
+    var myForm = document.getElementById("myForm");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    const request = await fetch("api/users/"+id, {
+        method: 'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    });
+    listar();
+    alertas("Se ha modificado el usuario Exitosamente!", 1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsurio')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+
+function verUsuario(id){
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    fetch("api/users/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+        
+        var cadena='';
+        
+        if(usuario){
+            cadena = '<div class="p-3 mb-2 bg-secondary text-white"><h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Visualizar Usuario Usuarios</h1></div>'+
+            '<ul class="list-group">'+
+            '<li class="list-group-item">Nombre:'+usuario.firstName+'</li>'+
+            '<li class="list-group-item">Apellido: '+usuario.lastName+'</li>'+
+            '<li class="list-group-item">Email: '+usuario.email+'</li>'+
+            '</ul>'+
+            '<br>'
+         }
+         document.getElementById("contentModal").innerHTML=cadena;
+         var myModal = new bootstrap.Modal(document.getElementById('modalUsurio'))
+         myModal.toggle();
+        
+    })
+}
+
+function alertas(mensaje,tipo){
+    var color=""; 
+    if(tipo == 1 ){//success verde
+        color = "success"
+
+    }
+    else{//danger rojo
+        color = "danger"
+
+    }
+    var alerta = '<div class="alert alert-'+color+' alert-dismissible fade show" role="alert">'+
+                   '<strong><i class="fa-solid fa-triangle-exclamation"></i></strong>'+ 
+                   mensaje+
+                   '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'+
+                 '</div>';
+    document.getElementById("datos").innerHTML=alerta;
+
+
 }
